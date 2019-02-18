ee73c68b9c25c6d85184795c36a0c1ece7df842d
eightfeet <bajiao000000@163.com>
2019-02-18 22:47:10 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,16 +6,14 @@ if (window.Promise === undefined) {
 import AddressPicker from './modules/AddressPicker';
 import { formatWheelsData } from '~/utils/regionsWheelsHelper.js';
 
-const data = formatWheelsData(window.BY_HEALTH_REGIONS);
+const regions = formatWheelsData(window.BY_HEALTH_REGIONS);
 
-window.newAddressPicker = new AddressPicker({
+window.newPicker = new AddressPicker({
 	trigger: '#example', // 触发Dom
 	title: '地区选择',
 	defaultValue: ['15', '1513', '151315'], // 默认选择的地址
-	triggerDisplayData: true, // 是否在"触发Dom"中显示已选数据 默认false(mobile-select中默认true)
-	wheels: [
-		{ data } // 原始数据
-	],
+	// triggerDisplayData: true, // 是否在"触发Dom"中显示已选数据 默认false(mobile-select中默认true)
+	regions, // 原始数据
 	// callback 确定后的回调，返回两个参数值，indexArr, data (分别代表当前选中wheels的位置和值)。
 	callback(indexArr, data){
 		console.log('callback', indexArr, data);
@@ -24,3 +22,9 @@ window.newAddressPicker = new AddressPicker({
 		console.log('取消', indexArr, data);
 	}
 });
+
+document.getElementById('exampleshow').onclick = function(){
+	// window.newPicker.showPicker(['19', '1922', '192215']);
+	window.newPicker.upDatePicker(['19', '1922', '192215']);
+};
+
--- src/modules/AddressPicker/index.js
+++ src/modules/AddressPicker/index.js
@@ -3,10 +3,30 @@ import { getPositionByDefaultValue } from '~/utils/regionsWheelsHelper.js';
 
 class AddressPicker extends MobileSelect {
 	constructor(data){
-		const { defaultValue, wheels } = data || {};
+		const { defaultValue, regions } = data || {};
+		const wheels = [
+			{ data: regions } // 原始数据
+		];
 		const operatedData = { triggerDisplayData: false, ...data, position: getPositionByDefaultValue(defaultValue, wheels), wheels };
 		console.log(operatedData.trigger);
 		super(operatedData);
+	}
+
+	setPositionById = (data) => {
+		this.initPosition = getPositionByDefaultValue(data, this.wheelsData);
+	}
+
+	upDatePicker = (data) => {
+		const willData = this.wheelsData[0].data;
+		this.setPositionById(data);
+		this.updateWheels(willData);
+	}
+
+	showPicker = (data) => {
+		if (Array.isArray(data) && data.length >= 2) {
+			this.upDatePicker(data);
+		}
+		this.show();
 	}
 }
 
